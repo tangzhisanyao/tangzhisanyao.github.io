b743f090b3c3ad23653a9b1841bcde7cad670214
tangzhisanyao <tangzhisanyao@users.noreply.github.com>
2023-10-19 15:01:05 +0000

    deploy: 7cfb4e62655840812f98f3cde1f948cf15db6edf

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/10/19/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/10/19/docker/","2023/10/19/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
