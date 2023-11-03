aeca17377d2cc983afdf89d329bee29215f1b6f8
tangzhisanyao <tangzhisanyao@users.noreply.github.com>
2023-11-03 05:11:56 +0000

    deploy: daa35426a35c4867fd00ab96aa16b38b44052e51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/10/23/Dart-数据类型/","2023/10/20/Git/","2023/10/23/SQLite/","2023/10/19/docker/","2023/10/19/flutter/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/10/23/Dart-数据类型/","2023/11/03/Flutter 打包/","2023/10/20/Git/","2023/10/23/SQLite/","2023/10/19/flutter/","2023/10/19/docker/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
